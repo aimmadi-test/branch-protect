96bb9b44d768c1cc96c1eec4de0185263403b21c
Arun <95721835+aimmadi8@users.noreply.github.com>
2021-12-20 15:38:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def webhook():
     # Store incoming json data from webhook
     payload = request.get_json()
-    user = "aimmadi8"
+    user = os.environ["GH_USERNAME"]
     cred = os.environ["GH_TOKEN"]
     if payload is None:
         print("POST was not formatted in JSON")
